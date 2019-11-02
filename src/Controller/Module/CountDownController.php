5bf48882e61fb75d5374dd28e537fb4425b9d3ab
szabolaszlo <laszlo.szabo@innonic.com>
2019-11-02 14:46:42 +0100

    Refactoring CountDown Module for Symfony

---
+++ src/Controller/Module/CountDownController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 2017. 01. 14.
+ * Time: 19:12
+ */
+
+namespace App\Controller\Module;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Qualify;
+use App\Entity\Race;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class CountDownController
+ * @package App\Controller\Module\CountDown
+ */
+class CountDownController extends AbstractController
+{
+    /**
+     * @return Response
+     * @throws \Exception
+     */
+    public function indexAction()
+    {
+        /** @var Qualify $qualify */
+        $qualify = $this->getDoctrine()->getRepository('App:Qualify')->getNextEvent();
+
+        /** @var Race $race */
+        $race = $this->getDoctrine()->getRepository('App:Race')->getNextEvent();
+
+        $now = new \DateTime();
+
+        return $this->render("controller/module/count_down/count_down.html.twig",
+            ['events' => [
+                'qualify' => array(
+                    'id' => $qualify->getType(),
+                    'name' => $qualify->getName(),
+                    'date' => $qualify->getDateTime()->format('M.d H:i'),
+                    'remain_time' => $now->diff($qualify->getDateTime())
+                ),
+                'race' => array(
+                    'id' => $race->getType(),
+                    'name' => $race->getName(),
+                    'date' => $race->getDateTime()->format('M.d H:i'),
+                    'remain_time' => $now->diff($race->getDateTime())
+                ),
+            ],
+                'details_link' => '/calendar',
+                'id' => 'count_down',
+            ]
+        );
+    }
+}
