754b6fa2e1461cd21bd1588056fd7e85fe0674d5
szabolaszlo <laszlo.szabo@innonic.com>
2019-12-28 13:38:24 +0100

    Result caching

--- src/Controller/Page/ResultsController.php
+++ src/Controller/Page/ResultsController.php
@@ -10,7 +10,10 @@
 namespace App\Controller\Page;
 
 use App\LegacyService\ResultTable\ResultTable;
+use Exception;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,20 +27,32 @@ class ResultsController extends AbstractController
      * @Route(path="/results", name="results", methods={"GET"})
      * @param ResultTable $resultTable
      * @return Response
-     * @throws \Exception
+     * @throws Exception
+     * @throws InvalidArgumentException
      */
     public function indexAction(ResultTable $resultTable)
     {
         $results = $this->getDoctrine()->getRepository('App:Result')->findAll();
 
-        $tables = [];
+        $cacheKey = 'results' . count($results);
 
-        foreach ($results as $result) {
-            $tables[] = $resultTable
-                ->getTable($this->getUser(), $result->getEvent(), 'full')
-                ->renderTable($result->getEvent());
+        $cache = new FilesystemAdapter();
+
+        $cachedItem = $cache->getItem($cacheKey);
+
+        if (!$cachedItem->isHit()) {
+            $tables = (array)$cachedItem->get();
+
+            foreach ($results as $result) {
+                $tables[] = $resultTable
+                    ->getTable($this->getUser(), $result->getEvent(), 'full')
+                    ->renderTable($result->getEvent());
+            }
+
+            $cachedItem->set($tables);
+            $cache->save($cachedItem);
         }
 
-        return $this->render('controller/page/results.html.twig', ['tables' => $tables]);
+        return $this->render('controller/page/results.html.twig', ['tables' => $cachedItem->get()]);
     }
 }
