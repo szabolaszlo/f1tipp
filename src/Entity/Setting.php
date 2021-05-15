5e73e8154d5410a00e057062ffecdcd722d13f18
szabolaszlo <laszlo.szabo@innonic.com>
2021-05-15 14:52:41 +0200

    Setable cover image

--- src/Entity/Setting.php
+++ src/Entity/Setting.php
@@ -31,6 +31,22 @@ class Setting
     /**
      * @return mixed
      */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getKey()
     {
         return $this->key;
