edc34a6066c4da1fbfbd162d40dc0cad714a80c4
szabolaszlo <laszlo.szabo@shoprenter.hu>
2022-02-19 13:01:17 +0100

    Some fixes for year 2022

--- src/Rule/RuleType/SprintQualify.php
+++ src/Rule/RuleType/SprintQualify.php
@@ -13,15 +13,13 @@ class SprintQualify extends ARule
      * @var array
      */
     protected $betAbleAttributes = array(
-        array('p1', 'driver', 3, 1),
-        array('p2', 'driver', 3, 1),
-        array('p3', 'driver', 3, 1),
-        array('p4', 'driver', 3, 1),
-        array('p5', 'driver', 3, 1),
-        array('p6', 'driver', 3, 1),
-        array('p7', 'driver', 3, 1),
-        array('p8', 'driver', 3, 1),
-        array('p9', 'driver', 3, 1),
-        array('p10', 'driver', 3, 1)
+        array('p1', 'driver', 1, 0),
+        array('p2', 'driver', 1, 0),
+        array('p3', 'driver', 1, 0),
+        array('p4', 'driver', 1, 0),
+        array('p5', 'driver', 1, 0),
+        array('p6', 'driver', 1, 0),
+        array('p7', 'driver', 1, 0),
+        array('p8', 'driver', 1, 0)
     );
 }
