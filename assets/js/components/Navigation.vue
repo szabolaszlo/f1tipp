085576c7e7d9b385d0b49123b8ac9116862f58ca
szabolaszlo <laszlo.szabo@shoprenter.hu>
2022-07-10 22:14:44 +0200

    Some redesign in actual module

--- assets/js/components/Navigation.vue
+++ assets/js/components/Navigation.vue
@@ -11,7 +11,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/"><img src="@images/logo.png" height="50" alt=""></a>
+          <a class="navbar-brand" href="/#/actual"><img src="@images/logo.png" height="50" alt=""></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="f1tipp-navbar-collapse">
