42c76d31601bde0bb475c0bad090017a56ddf2d3
szabolaszlo <laszlo.szabo@shoprenter.hu>
2023-02-26 12:36:03 +0100

    2023 points and prices

--- src/Calculator/Provider/PointProvider.php
+++ src/Calculator/Provider/PointProvider.php
@@ -15,28 +15,17 @@ namespace App\Calculator\Provider;
 class PointProvider
 {
 
-    const PLACE_1 = 18;
-    const PLACE_2 = 13;
-    const PLACE_3 = 11;
-    const PLACE_4 = 8;
-    const PLACE_5 = 7;
-    const PLACE_6 = 6;
-    const PLACE_7 = 4;
+    const PLACE_1 = 14;
+    const PLACE_2 = 10;
+    const PLACE_3 = 8;
+    const PLACE_4 = 7;
+    const PLACE_5 = 6;
+    const PLACE_6 = 4;
+    const PLACE_7 = 3;
     const PLACE_8 = 2;
     const PLACE_9 = 1;
     const PLACE_10 = 0;
-/*
-    const PLACE_1 = 19;
-    const PLACE_2 = 13;
-    const PLACE_3 = 7;
-    const PLACE_4 = 5;
-    const PLACE_5 = 3;
-    const PLACE_6 = 2;
-    const PLACE_7 = 1;
-    const PLACE_8 = 0;
-    const PLACE_9 = 0;
-    const PLACE_10 = 0;
-*/
+
     protected $placePoints = array(
         1 => self::PLACE_1,
         2 => self::PLACE_2,
